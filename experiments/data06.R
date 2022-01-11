6e28c4b3c75535b81859a25d9306e4439ba49a24
Noelia Rico <noeliarico@uniovi.es>
2022-01-11 12:10:34 +0100

    Results for data06

--- experiments/data06.R
+++ experiments/data06.R
@@ -1,61 +1,61 @@
 # Number of clusters
-nc <- length(unique(data04))
-plot_hclust_comparison(data04, nc, mode = "sca")
+nc <- length(unique(data06$class))
+plot_hclust_comparison(data06, nc, mode = "sca")
 
 # Create a subset
-set.seed(3)
-data04b <- caret::createDataPartition(
-  data04$class,
-  p = .4,
+set.seed(5)
+data06b <- caret::createDataPartition(
+  data06$class,
+  p = .04,
   list = F
 )
-data04b <- data04[data04b,]
-data04_training <- data04b[,1:2]
+data06b <- data06[data06b,]
+data06_training <- data06b[,1:2]
 
 # Check the new visualization
-pcsca <- plot_hclust_comparison(data04b, nc, mode = "sca")
+pcsca <- plot_hclust_comparison(data06b, nc, mode = "sca")
 pcsca
 
-data04_s <- hclust(dist(data04_training), method = "single")
-data04_c <- hclust(dist(data04_training), method = "complete")
-data04_a <- hclust(dist(data04_training), method = "average")
+data06_s <- hclust(dist(data06_training), method = "single")
+data06_c <- hclust(dist(data06_training), method = "complete")
+data06_a <- hclust(dist(data06_training), method = "average")
 
 ######## PLURALITY #############################################################
 
-data04_sc_plurality <- mc_hclust(data04_training,
+data06_sc_plurality <- mc_hclust(data06_training,
                                  linkage_methods = c("single", "complete"),
                                  aggregation_method = "plurality",
                                  verbose = F)
 
-data04_sca_plurality <- mc_hclust(data04_training,
+data06_sca_plurality <- mc_hclust(data06_training,
                                   linkage_methods = c("single", "complete", "average"),
                                   aggregation_method = "plurality",
                                   verbose = F)
 
-plot_mchclust_tiles(data04_sc_plurality, 10) + ggtitle("SC")+ plot_mchclust_tiles(data04_sca_plurality, 10) + ggtitle("SCA")
+plot_mchclust_tiles(data06_sc_plurality, 10) + ggtitle("SC")+ plot_mchclust_tiles(data06_sca_plurality, 10) + ggtitle("SCA")
 
 ######## TAPPROVAL #############################################################
 
-data04_sc_tapproval <- mc_hclust(data04_training,
+data06_sc_tapproval <- mc_hclust(data06_training,
                                  linkage_methods = c("single", "complete"),
                                  aggregation_method = nc,
                                  verbose = F)
 
-data04_sca_tapproval <- mc_hclust(data04_training,
+data06_sca_tapproval <- mc_hclust(data06_training,
                                   linkage_methods = c("single", "complete", "average"),
                                   aggregation_method = nc,
                                   verbose = F)
 
-plot_mchclust_tiles(data04_sc_tapproval, 10) + ggtitle("SC")+ plot_mchclust_tiles(data04_sca_tapproval, 10) + ggtitle("SCA")
+plot_mchclust_tiles(data06_sc_tapproval, 10) + ggtitle("SC")+ plot_mchclust_tiles(data06_sca_tapproval, 10) + ggtitle("SCA")
 
 ######## BORDA #################################################################
 
-data04_sc_borda <- mc_hclust(data04_training,
+data06_sc_borda <- mc_hclust(data06_training,
                              linkage_methods = c("single", "complete"),
                              aggregation_method = "borda",
                              verbose = F)
 
-data04_sca_borda <- mc_hclust(data04_training,
+data06_sca_borda <- mc_hclust(data06_training,
                               linkage_methods = c("single", "complete", "average"),
                               aggregation_method = "borda",
                               verbose = F)
@@ -64,7 +64,7 @@ data04_sca_borda <- mc_hclust(data04_training,
 
 # Save results
 
-save(data04_sc_plurality, data04_sca_plurality,
-     data04_sc_tapproval, data04_sca_tapproval,
-     data04_sc_borda, data04_sca_borda,
-     file = "experiments/results/results_data04.RData")
+save(data06_sc_plurality, data06_sca_plurality,
+     data06_sc_tapproval, data06_sca_tapproval,
+     data06_sc_borda, data06_sca_borda,
+     file = "experiments/results/results_data06.RData")
